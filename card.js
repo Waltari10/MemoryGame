1a83a29c9ffb6f92333d4bdb009c2764a7a7db25
Valtteri <vallai10@gmail.com>
2016-04-10 00:01:07 +0300

    matching and reverting cards works

--- card.js
+++ card.js
@@ -1,31 +1,41 @@
+
 function card(id) {
+	"use strict";
 	this.id = id;
 	this.flipped = false;
 	this.image = null;
 
 	this.flip = function() {
 
-		$("#" + this.cardID).flip({
-			content: this.getBackContent(),
-			onEnd: this.onFlipComplete()
-		});
-
-		console.log("flipped");
-
-		$("#" + this.id + " img").show();
-		
-		this.flipped = true;
-
+		if ($("#" + id).hasClass("flipped")) {
+			$("#" + id).removeClass("flipped");
+		} else {
+			$("#" + id).addClass("flipped");
+		}
 	};
 
-	this.revertFlip = function () {
+	this.initialFlip = function () {
+		if ($("#" + id).hasClass("flipped")) {
+			$("#" + id).removeClass("flipped");
+		} else {
+			$("#" + id).addClass("flipped");
+		}
+	}
 
-		$("#" + this.id + " img").hide();
-		
-		$("#" + this.id).revertFlip();
+	
 
-		this.flipped = false;
-	};
+
+	this.addOnClick = function() {
+		$("#" + id).click(this.flip);
+	}
+
+	this.offFlipping = function() {
+		$("#" + id).off('click');
+	}
+
+	this.onFlipping = function() {
+		$("#" + id).on('click');
+	}
 
 	this.getHTML = function () {
 		return '<div id="' + this.id + '" class="card flipped"> <figure class="back">' + "<img src='etu.jpg'>" + '</figure> <figure class="front">' + "<img src='" + this.image + "'>" +  '</figure> </div>';
--- scripts.js
+++ scripts.js
@@ -1,77 +1,107 @@
-var cards = new Array();
-var numberArray = new Array(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10);
-
 $(document).ready(function() {
-     
+	"use strict";
+
+	var cards;
+	var numberArray = Array(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10);
+	var lastFlippedCard = null;
+
     $('#startGameButton').click(function() {
-    	 $('#board').empty();
-         shuffle(numberArray);
-         initcards();
-         setTimeout(flipAllCards, 1000);
-         setTimeout(flipAllCards, 2000);
-         setTimeout(enableCards, 3000);
-
+		$('#board').empty();
+		shuffle(numberArray);
+		createCards();
+		setTimeout(flipAllCards, 1000);
+		setTimeout(flipAllCards, 2000);
+		setTimeout(enableAllFlipping, 3000);
+		setTimeout(addGameLogic, 3000);
     });
-});
 
-function flipAllCards(){
-	console.log("test");
-	for (var i = 0; i < 20; i++) {
-		if ($("#card" + i).hasClass("flipped")) {
-			$("#card" + i).removeClass("flipped");
+    function addGameLogic () {
+    	for (var i = 0; i < 20; i++) {
+    		$("#card" + i).click(checkFlippings);
+    	}
+	}
+
+	function checkFlippings() {
+
+
+		if ($(this).hasClass('flipped')) {
+			console.log("User is reverting his choice.");
+			lastFlippedCard = null;
 		} else {
-			$("#card" + i).addClass("flipped");
+			console.log("user is peeking into a card");
+			if (lastFlippedCard === null) {
+				lastFlippedCard = $(this);
+			} else {
+				console.log("users second peek. Here is potential money!");
+
+				var temp = $(this).find("figure.front").find("img").attr("src");
+
+				if (lastFlippedCard.find("figure.front").find("img").attr("src") === temp) {
+					console.log("its a match! freeze cards");
+					$(this).off('click');
+					$(lastFlippedCard).off('click');
+					lastFlippedCard = null;
+				} else {
+					$(this).addClass('flipped');
+					$(lastFlippedCard).addClass('flipped');
+					lastFlippedCard = null;
+				}
+			}
+
 		}
 	}
-}
 
-function enableCards() {
-	console.log("NYTT");
-	for (var i = 0; i < 20; i++) {
-		$("#card" + i).click(flipCard(i));
-	}
-}
 
-function flipCard(i){
-	return function () {
-			if ($("#card" + i).hasClass("flipped")) {
-				$("#card" + i).removeClass("flipped");
-			} else {
-				$("#card" + i).addClass("flipped");
-			}
-	}
-}
-
-function initcards() {
-	var counter = 0,
-		tempCard = null
-		cards = new Array();
-
-	for (counter = 0; counter < 20; counter++){
-		tempCard = createCard(counter);
-		cards.push(tempCard);
+	function flipAllCards(){
+		for (var i = 0; i < 20; i++) {
+			cards[i].initialFlip();
+		}
 	}
 
-	shuffle(cards);
-
-	for (counter = 0; counter < 20; counter++){
-		$('#board').append(cards[counter].getHTML());
+	function disableAllFlipping() {
+		for (var i = 0; i < 20; i++) {
+			cards[i].offFlipping();
+		}
 	}
-}
 
-function createCard(counter) {
-	tempCard = new card("card" + counter);
-	tempCard.setImage(numberArray[counter]);
-	return tempCard;
-}
+	function enableAllFlipping() {
+		for (var i = 0; i < 20; i++) {
+			cards[i].addOnClick();
+		}
+	}
 
+	function createCards() {
+		var counter = 0,
+			tempCard = null;
+			cards = [];
 
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i -= 1) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
-}
+		for (counter = 0; counter < 20; counter++){
+			tempCard = createCard(counter);
+			cards.push(tempCard);
+		}
+
+		shuffle(cards);
+
+		for (counter = 0; counter < 20; counter++){
+			$('#board').append(cards[counter].getHTML());
+		}
+	}
+
+	function createCard(counter) {
+		var tempCard = new card("card" + counter);
+		tempCard.setImage(numberArray[counter]);
+		return tempCard;
+	}
+
+	function shuffle(a) {
+	    var j, x, i;
+	    for (i = a.length; i; i -= 1) {
+	        j = Math.floor(Math.random() * i);
+	        x = a[i - 1];
+	        a[i - 1] = a[j];
+	        a[j] = x;
+	    }
+	}
+
+});
+
