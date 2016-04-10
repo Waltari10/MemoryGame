1c55d71067ff799332e4d8412e911b267ebf72da
Valtteri <vallai10@gmail.com>
2016-04-10 09:05:11 +0300

    asdf

--- scripts.js
+++ scripts.js
@@ -4,39 +4,51 @@ $(document).ready(function() {
 	var cards;
 	var numberArray = Array(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10);
 	var lastFlippedCard = null;
-
+	var revealTime = 4000;
+	var cardAmount = 20;
+	
     $('#startGameButton').click(function() {
-		$('#board').empty();
-		shuffle(numberArray);
-		createCards();
-		setTimeout(flipAllCards, 1000);
-		setTimeout(flipAllCards, 2000);
-		setTimeout(enableAllFlipping, 3000);
-		setTimeout(addGameLogic, 3000);
 
-		setTimeout(areAllCardsMatched, 3000);
+    	disableButton($(this), 5000);
+    	initGame();
+		
     });
 
+    function initGame(){
+    	$('#board').empty();
+		shuffle(numberArray);
+		createCards();
+		setTimeout(flipAllCards, 500);
+		setTimeout(flipAllCards, revealTime);
+		setTimeout(enableAllFlipping, revealTime + 500);
+		setTimeout(addGameLogic, revealTime + 500);
+    }
+
+    function disableButton(btn, time) {
+    	btn.prop('disabled', true);
+    	setTimeout(function() {
+    		btn.prop('disabled', false);
+    	}, time);
+    }
+
     function addGameLogic () {
-    	for (var i = 0; i < 20; i++) {
+    	for (var i = 0; i < cardAmount; i++) {
     		$("#card" + i).click(checkFlippings);
     	}
 	}
 
 	function checkFlippings() {
-
-
 		if ($(this).hasClass('flipped')) {
-			console.log("User is reverting his choice.");
+			//console.log("User is reverting his choice.");
 			lastFlippedCard = null;
 		} else {
-			console.log("user is peeking into a card");
+			//console.log("user is peeking into a card");
 			if (lastFlippedCard === null) {
 				lastFlippedCard = $(this);
 			} else {
-				console.log("users second peek. Here is potential money!");
+				//console.log("users second peek. Here is potential money!");
 				if (lastFlippedCard.find("figure.front").find("img").attr("src") === $(this).find("figure.front").find("img").attr("src")) {
-					console.log("its a match! freeze cards");
+					//console.log("its a match! freeze cards");
 					$(this).off('click').removeClass('clickable');
 					$(lastFlippedCard).off('click').removeClass('clickable');
 					
@@ -49,7 +61,7 @@ $(document).ready(function() {
 					setTimeout(function () {
 						temp1.addClass('flipped');
 						temp2.addClass('flipped');
-					}, 2000);
+					}, cardAmount00);
 				}
 				lastFlippedCard = null;
 			}
@@ -58,29 +70,30 @@ $(document).ready(function() {
 	}
 
 	function areAllCardsMatched() {
-		for (var i = 0; i < 20; i++) {
+		for (var i = 0; i < cardAmount; i++) {
 			if ($("#card" + i).hasClass('clickable') === true) {
 
 				return false;
 			}
-		}		
+		}
+		alert("You won!");
 		return true;
 	}
 
 	function flipAllCards(){
-		for (var i = 0; i < 20; i++) {
+		for (var i = 0; i < cardAmount; i++) {
 			cards[i].initialFlip();
 		}
 	}
 
 	function disableAllFlipping() {
-		for (var i = 0; i < 20; i++) {
+		for (var i = 0; i < cardAmount; i++) {
 			cards[i].offFlipping();
 		}
 	}
 
 	function enableAllFlipping() {
-		for (var i = 0; i < 20; i++) {
+		for (var i = 0; i < cardAmount; i++) {
 			cards[i].addOnClick();
 		}
 	}
@@ -90,14 +103,14 @@ $(document).ready(function() {
 			tempCard = null;
 			cards = [];
 
-		for (counter = 0; counter < 20; counter++){
+		for (counter = 0; counter < cardAmount; counter++){
 			tempCard = createCard(counter);
 			cards.push(tempCard);
 		}
 
 		shuffle(cards);
 
-		for (counter = 0; counter < 20; counter++){
+		for (counter = 0; counter < cardAmount; counter++){
 			$('#board').append(cards[counter].getHTML());
 		}
 	}
