aec731039a8a035f0c4afb62c5cf1e9fd4b2f8dc
Waltari10 <vallai10@gmail.com>
2016-05-02 20:09:09 +0300

    uuuuu

--- test/js/scripts.js
+++ test/js/scripts.js
@@ -6,14 +6,9 @@ $(document).ready(function () {
     function flipFunction() {
         if (flipped) {
             flipped = false;
-            card.removeClass('flipAndZoom');
             card.removeClass('flip');
         } else {
             card.addClass("flip");
-    
-            setTimeout(function () {
-                card.addClass("flipAndZoom");
-            }, 1000);
             flipped = true;
         }
     };
