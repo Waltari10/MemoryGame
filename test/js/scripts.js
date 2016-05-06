df24d3a9190902dc14da36c817dd1e6afb1fb88d
Waltari10 <vallai10@gmail.com>
2016-05-06 13:29:55 +0300

    asdf

--- test/js/scripts.js
+++ test/js/scripts.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
     var flipped = false;
     var card = $("#card0");
-    card.click(function() { flipFunction();});
+    var figureFront = $("#card0 .front");
+    var figureBack = $("#card0 .back");
+    card.click(function() { 
+        flipFunction();});
     
     function flipFunction() {
-        if (flipped) {
+        if (flipped) { //card flipped so front is invisible and back is visible. 
             flipped = false;
+            
+            figureFront.removeClass('invisible');
+
+            figureBack.removeClass('visible');
+            
+            
             card.removeClass('flip');
-        } else {
-            card.addClass("flip");
+        } else {  //card not flipped so front is visible and back is invisible
             flipped = true;
+            
+            figureFront.removeClass('visible');
+            figureFront.addClass('invisible');
+
+            figureBack.addClass('visible');
+            figureBack.removeClass('invisible');
+            
+            card.addClass('flip');
         }
     };
 });
